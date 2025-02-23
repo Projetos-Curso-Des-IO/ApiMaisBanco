c18471c1b427a3b051994b9fdd1cb02bb1aefaa8
Josiel <cjosiel2@gmail.com>
2025-02-23 17:08:47 -0300

    Mapeamentos gerados

--- Data/ApiDbContext.cs
+++ Data/ApiDbContext.cs
@@ -1,11 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ApiFuncional.Model;
+using Microsoft.EntityFrameworkCore;
 
 namespace ApiFuncional.Data
 {
 	public class ApiDbContext : DbContext
 	{
 		public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options) 
-		{
+		{			
 		}
+		public DbSet<Produto> Produtos { get; set; }
 	}
 }
